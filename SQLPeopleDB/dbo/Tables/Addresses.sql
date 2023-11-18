702bdaee2407300cc0ada80930be1ae664d994ce
naomifleurnoel <nperenboom@hotmail.com>
2023-11-18 17:08:54 +0100

    Added the addresses table and edits to people table

---
+++ SQLPeopleDB/dbo/Tables/Addresses.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Addresses]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AddressName] VARCHAR(50) NOT NULL, 
+    [HouseNumber] VARCHAR(10) NOT NULL, 
+    [Zipcode] VARCHAR(7) NOT NULL, 
+    [City] VARCHAR(50) NOT NULL, 
+    [Country] VARCHAR(50) NOT NULL
+)
