26168cf739d3fad036ea3719a686306e1a66c2ab
naomifleurnoel <nperenboom@hotmail.com>
2023-11-18 20:53:35 +0100

    added connection setting/string

--- SQLServerUI/Program.cs
+++ SQLServerUI/Program.cs
@@ -1,2 +1,26 @@
-﻿Console.ReadLine();
+﻿using Microsoft.Extensions.Configuration;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(GetConnectionString());
+
+        Console.ReadLine(); 
+    }
+
+    private static string GetConnectionString(string connectionStringName = "Default")
+    {
+        string output = "";
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        var config = builder.Build();
+
+        output = config.GetConnectionString(connectionStringName);
+
+        return output;
+    }
+}
