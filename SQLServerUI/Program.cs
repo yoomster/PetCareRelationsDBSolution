1243bc1ee17e6bfa27c39df24aa43fb7ec9d8985
naomifleurnoel <nperenboom@hotmail.com>
2023-11-19 00:11:15 +0100

    added method for READING

--- SQLServerUI/Program.cs
+++ SQLServerUI/Program.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DataAccessLibrary;
+using Microsoft.Extensions.Configuration;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        SqlCrud sql = new SqlCrud(GetConnectionString());
+
+        ReadAllContacts(sql);
+
 
         Console.ReadLine(); 
     }
@@ -22,4 +27,22 @@ internal class Program
 
         return output;
     }
+
+    private static void ReadAllContacts(SqlCrud sql)
+    {
+        var rows = sql.GetAllContacts();
+
+        foreach (var row in rows)
+        {
+            Console.WriteLine($"{row.Id}: {row.FirstName} {row.LastName}");
+        }
+    }
+
+    private static void ReadContact(SqlCrud sql, int contactId)
+    {
+        var contact = sql.GetFullContactById(contactId);
+
+        Console.WriteLine($"{contact.BasicInfo.Id}: {contact.BasicInfo.FirstName} {contact.BasicInfo.LastName}");
+    }
+
 }
